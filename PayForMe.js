a6dda78418924b1270d722efc1fc5321dc608b99
Kourylenko <Kourylenko@gmail.com>
2018-11-13 21:29:05 +0200

    up

--- PayForMe.js
+++ PayForMe.js
@@ -1,33 +1,65 @@
-var names = ["Tanya", "Glib", "Olga"];
-var whoPaid = document.querySelector("#nameWhoPay");
+var names = [];
+var whoPaid = names[0];
 var howMuch = 300;
 var forWho = ["Tanya", "Glib", "Olga"];
 
 
-document.querySelector("button").addEventListener("click", function() {
-
+document.querySelector("#addName").addEventListener("click", function() {
     // add input value in arr element
     var namesData = document.querySelector("#namesData").value;
     names.push(namesData);
-
+    
     //create li and add text (from input )
-    var ol = document.getElementById("data");
+    var olName = document.getElementById("data");
+    /*
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(namesData));
-    ol.appendChild(li);
-
-     // creat select->options with names from arr
-    var select = document.getElementById("nameWhoPay");
-    var option = document.createElement("option");
-    select.appendChild(option);
-    option.appendChild(document.createTextNode(namesData));
-    names[i] == whoPaid.selectedIndex;
+    olName.appendChild(li);
+    */
+    createElement("li", olName, namesData);
 });
 
+document.querySelector("#goCalcul").addEventListener("click", function() {
+    // On click, find select. 
+    var select = document.getElementById("nameWhoPay");
+    var ulDebtors = document.querySelector("#debtors");
+    // then loop through names Arr 
+    for (var i = 0; i < names.length; i++) {
+        // then call f to put value of current element arr 
+        createElement("option", select, names[i]);
+        createElement("li", ulDebtors, names[i]);
+    }
+});
+/*
+//f create option and appends it to select each time when you call this f
+function createOption(name, select) {
+    var option = document.createElement("option");
+    select.appendChild(option);
+    option.appendChild(document.createTextNode(name));
+}
 
-	// whoPaid.addEventListener("click", function() {
-	
-	// });
+//f create li and appends it to ul each time when you call this f
+function createLi(name, liContainer) {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(name));
+    liContainer.appendChild(li);
+}
+*/
+
+/*
+к нам приходит название тега который хотим создать,
+к нам приходит обьект ХТМЛ в который хотим запихнуть
+к нам приходит масив нейм
+
+1. создаём елемент согласно текнейм который пришел
+2. аппендим созданы й елемент в нутрь пришедшего из елемента контейнера
+3. если есть нейм мы припихываеим к нему нейм
+*/
+function createElement(elementName, container, data) {
+    var element = document.createElement(elementName);
+    container.appendChild(element);
+    element.appendChild(document.createTextNode(data));
+}
 
 
 
